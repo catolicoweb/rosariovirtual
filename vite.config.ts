f97c954cee120ebb52bf99d8fc8f1b753d8c1e96
catolicoweb <catolicoweb@users.noreply.github.com>
2026-01-29 19:29:10 -0300

    Update vite base path configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    base: mode === 'production' ? '/rosariovirtual/' : '/',
+    base: '/',
     plugins: [react(), tailwindcss()],
   }
 })
